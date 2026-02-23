6c9aa12f341ce683e8973a49e9e21b60dbfefedf
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:25:58 +0300

    Create ProductionTask.cs

---
+++ ProductionTask.cs
@@ -0,0 +1,9 @@
+namespace IndustrialExpansionMRLONE
+{
+    public class ProductionTask
+    {
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int FinishDay { get; set; }
+    }
+}
