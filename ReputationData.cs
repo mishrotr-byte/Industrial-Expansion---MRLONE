d28035f7bcc4e44ac65b0edc56750f1745a3e069
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:39:11 +0300

    Create ReputationData.cs

---
+++ ReputationData.cs
@@ -0,0 +1,11 @@
+namespace IndustrialExpansionMRLONE.Reputation
+{
+    public class ReputationData
+    {
+        public int Level { get; set; } = 1;
+        public int Experience { get; set; } = 0;
+        public int Influence { get; set; } = 0;
+        public int MarketShare { get; set; } = 1;
+        public int Trust { get; set; } = 50;
+    }
+}
