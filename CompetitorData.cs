311541f1629e269026ae2110faa38de602ea4ae4
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:43:15 +0300

    Create CompetitorData.cs

---
+++ CompetitorData.cs
@@ -0,0 +1,9 @@
+namespace IndustrialExpansionMRLONE.AI
+{
+    public class CompetitorData
+    {
+        public int Capital { get; set; } = 50000;
+        public float MarketShare { get; set; } = 10f;
+        public int Aggressiveness { get; set; } = 1;
+    }
+}
