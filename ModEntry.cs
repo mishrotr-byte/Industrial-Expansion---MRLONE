7f48934e40a3e11a8df7cc609bea666d55472274
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:31:53 +0300

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -70,3 +70,68 @@ private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         Game1.activeClickableMenu = new FactoryTerminalMenu();
     }
 }
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using IndustrialExpansionMRLONE.Factory;
+using IndustrialExpansionMRLONE.Economy;
+using IndustrialExpansionMRLONE.UI;
+using IndustrialExpansionMRLONE.Networking;
+using IndustrialExpansionMRLONE.Core;
+
+namespace IndustrialExpansionMRLONE
+{
+    public class ModEntry : Mod
+    {
+        internal static ModEntry Instance = null!;
+        internal static ModConfig Config = null!;
+
+        internal FactoryManager Factory = null!;
+        internal EconomyManager Economy = null!;
+        internal MultiplayerSync Net = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            Instance = this;
+            Config = helper.ReadConfig<ModConfig>();
+
+            Factory = new FactoryManager(helper);
+            Economy = new EconomyManager(helper);
+            Net = new MultiplayerSync(helper);
+
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+            helper.Events.GameLoop.Saving += OnSaving;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            Factory.Load();
+            Economy.Load();
+        }
+
+        private void OnSaving(object? sender, SavingEventArgs e)
+        {
+            Factory.Save();
+            Economy.Save();
+        }
+
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
+        {
+            Factory.ProcessProduction();
+            Economy.ProcessDaily();
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button == SButton.F6)
+            {
+                Game1.activeClickableMenu = new FactoryMenu();
+            }
+        }
+    }
+}
