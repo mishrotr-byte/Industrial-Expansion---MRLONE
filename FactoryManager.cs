092cb9b5afa93f53f5cbec6e49f9c5288f12ecd9
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:27:07 +0300

    Update FactoryManager.cs

--- FactoryManager.cs
+++ FactoryManager.cs
@@ -1,6 +1,7 @@
 using StardewModdingAPI;
 using StardewValley;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IndustrialExpansionMRLONE
 {
@@ -8,33 +9,73 @@ namespace IndustrialExpansionMRLONE
     {
         private readonly IModHelper Helper;
 
-        private Dictionary<string, int> ProductionQueue = new();
+        private FactorySaveData Data = new();
 
         public FactoryManager(IModHelper helper)
         {
             Helper = helper;
         }
 
-        public void StartProduction(string itemId, int days)
+        public void Load()
         {
-            ProductionQueue[itemId] = Game1.Date.TotalDays + days;
+            Data = Helper.Data.ReadSaveData<FactorySaveData>("factory-data") ?? new FactorySaveData();
+        }
+
+        public void Save()
+        {
+            Helper.Data.WriteSaveData("factory-data", Data);
+        }
+
+        public void AddProduct(CustomProduct product)
+        {
+            Data.Products.Add(product);
+            Save();
+        }
+
+        public void StartProduction(string productId, int quantity)
+        {
+            if (!Context.IsMainPlayer)
+                return;
+
+            var product = Data.Products.FirstOrDefault(p => p.Id == productId);
+            if (product == null) return;
+
+            Data.ActiveTasks.Add(new ProductionTask
+            {
+                ProductId = productId,
+                Quantity = quantity,
+                FinishDay = Game1.Date.TotalDays + product.ProductionDays
+            });
+
+            Save();
         }
 
         public void ProcessProduction()
         {
-            List<string> finished = new();
+            if (!Context.IsMainPlayer)
+                return;
 
-            foreach (var entry in ProductionQueue)
+            var finished = Data.ActiveTasks
+                .Where(t => Game1.Date.TotalDays >= t.FinishDay)
+                .ToList();
+
+            foreach (var task in finished)
             {
-                if (Game1.Date.TotalDays >= entry.Value)
+                var product = Data.Products.First(p => p.Id == task.ProductId);
+
+                for (int i = 0; i < task.Quantity; i++)
                 {
-                    Game1.player.addItemByMenuIfNecessaryElseHoldUp(ItemRegistry.Create(entry.Key));
-                    finished.Add(entry.Key);
+                    Game1.player.addItemByMenuIfNecessaryElseHoldUp(product.CreateObject());
                 }
+
+                product.Popularity += task.Quantity;
+                Data.ActiveTasks.Remove(task);
             }
 
-            foreach (var f in finished)
-                ProductionQueue.Remove(f);
+            Save();
         }
+
+        public List<CustomProduct> GetProducts() => Data.Products;
+        public List<ProductionTask> GetTasks() => Data.ActiveTasks;
     }
 }
