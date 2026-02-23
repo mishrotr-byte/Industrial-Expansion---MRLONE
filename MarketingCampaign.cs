baded7ccaaabd4351137678f452108b3a8f7486a
mishrotr-byte <mishrotr@gmail.com>
2026-02-23 10:44:58 +0300

    Create MarketingCampaign.cs

---
+++ MarketingCampaign.cs
@@ -0,0 +1,10 @@
+namespace IndustrialExpansionMRLONE.Marketing
+{
+    public class MarketingCampaign
+    {
+        public string Name { get; set; } = "";
+        public int RemainingDays { get; set; }
+        public float DemandBoost { get; set; }
+        public int Cost { get; set; }
+    }
+}
